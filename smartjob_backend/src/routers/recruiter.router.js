ba05452be876ff16571eb25ca54108a350165e52
Satyamray21 <satyamray43@gmail.com>
2025-06-18 14:37:02 +0530

    added router for recruiter.

---
+++ smartjob_backend/src/routers/recruiter.router.js
@@ -0,0 +1,14 @@
+import {Router} from "express";
+import {createRecruiter} from "../controllers/recruiter.controller.js";
+import {upload} from "../middlewares/imageMulter.middleware.js";
+const router = Router();
+router.route("/").post(
+    upload.fields([
+        {
+            name:"profileImage",
+            maxCount:1
+        }                 
+   ]),
+   createRecruiter
+)
+export default router;
