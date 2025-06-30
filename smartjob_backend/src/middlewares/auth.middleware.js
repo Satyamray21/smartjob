830ef0fe3ebb0fd1bb5e4e1ef63eca1bd9aae808
Satyamray21 <satyamray43@gmail.com>
2025-06-30 13:33:37 +0530

    added auth middleware.

---
+++ smartjob_backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import { Recruiter } from "../models/recruiter.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import {asyncHandler} from "../utils/asyncHandler.js";
+
+export const verifyRecruiter = asyncHandler(async (req, res, next) => {
+  // Check token from cookies or Authorization header
+  const token =
+    req.cookies?.accessToken ||
+    req.headers.authorization?.replace("Bearer ", "").trim();
+
+  if (!token) {
+    throw new ApiError(401, "Access denied. No token provided");
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    
+    const recruiter = await Recruiter.findById(decoded.id).select("-password");
+    
+    if (!recruiter) {
+      throw new ApiError(404, "Recruiter not found");
+    }
+
+    if (recruiter.isBlocked || recruiter.isInactive) {
+      throw new ApiError(403, "Your account is not active");
+    }
+
+    req.user = recruiter; 
+    next();
+  } catch (error) {
+    throw new ApiError(401, "Invalid or expired token", [error.message]);
+  }
+});
